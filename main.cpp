9fbe623e164ce2f711168bdc5b24b48e91e60e7e
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-21 12:36:34 -0300

    Basic SFML project with CMake

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "config.h"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(int argc, char* argv[]) {
+    cout << "Version " << ball_VERSION_MAJOR << "." << ball_VERSION_MINOR << endl;
+
+    sf::Window App(sf::VideoMode(800, 600), "Ball");
+
+    while (App.isOpen()) {
+        sf::Event Event;
+        while (App.pollEvent(Event)) {
+            if (Event.type == sf::Event::Closed) {
+                App.close();
+            }
+            App.display();
+        }
+    }
     return 0;
 }
