0acafe392a406d972aaa5f3fce04fa6cf372568a
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-21 19:14:18 -0300

    Draw OpenGL Window

--- main.cpp
+++ main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include "config.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>S
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
     cout << "Version " << ball_VERSION_MAJOR << "." << ball_VERSION_MINOR << endl;
 
-    sf::Window App(sf::VideoMode(800, 600), "Ball");
+    // Create the window
+    sf::Window window(sf::VideoMode(800, 600), "Ball", sf::Style::Default, sf::ContextSettings(32));
+    window.setVerticalSyncEnabled(true);
 
-    while (App.isOpen()) {
-        sf::Event Event;
-        while (App.pollEvent(Event)) {
-            if (Event.type == sf::Event::Closed) {
-                App.close();
+    // load resources, initialize OpenGL states etc
+
+    // main loop
+    bool running = true;
+    while (running) {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                running = false;
+            } else if (event.type == sf::Event::Resized) {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
             }
-            App.display();
         }
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw
+
+        // end the current frame (internally, swaps the front and back buffers)
+        window.display();
     }
+
+    // release the resources here
+
     return 0;
 }
