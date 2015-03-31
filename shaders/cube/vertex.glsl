29881e54ee4d1b82e4b46173399db2e094f68081
Pedro Vanzella <pedro@pedrovanzella.com>
2015-03-31 20:17:05 -0300

    Updated Cube Shaders

--- shaders/cube/vertex.glsl
+++ shaders/cube/vertex.glsl
@@ -1,7 +1,13 @@
 #version 400
 
-in vec3 vp;
+layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec3 vertexColor;
+
+out vec3 fragmentColor;
+uniform mat4 MVP;
 
 void main() {
-     gl_Position = vec4(vp, 1.0);
+     gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+
+     fragmentColor = vertexColor;
 }
--- states/cube_state.cpp
+++ states/cube_state.cpp
@@ -52,10 +52,32 @@ namespace Dolly {
 	vao = 0;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
+	static GLfloat g_color_buffer_data[12*3*3];
+	for (int v = 0; v < 12*3; v++) {
+	    g_color_buffer_data[3*v+0] = 0.5;
+	    g_color_buffer_data[3*v+1] = 1.0;
+	    g_color_buffer_data[3*v+2] = 0.0;
+	}
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glVertexAttribPointer(
+	    1,         // attribute. No particular reason for 1, but must match the layout in the shader.
+	    3,         // size
+	    GL_FLOAT,  // type
+	    GL_FALSE,  // normalized?
+	    0,         // stride
+	    (void*)0   // array buffer offset
+	    );
+	
 	shaderLoader = new ShaderLoader("shaders/cube/vertex.glsl", "shaders/cube/fragment.glsl");
     }
 
